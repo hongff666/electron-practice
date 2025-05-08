9609919bc674ac86e19734f164dc25ac6c43ecf6
hhl206 <hhl206@qq.com>
2025-05-08 17:13:04 +0800

    添加 lucide-vue-next 依赖，更新 App.vue 以使用 Lucide 图标并调整界面，重构 ping 功能以显示版本信息

--- src/render/App.vue
+++ src/render/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="flex-1 flex items-center justify-center">
-    <div class="shadow-lg rounded-lg p-4 bg-white flex flex-col gap-2">
-      <div class="text-2xl font-bold mb-4">
-        ChromeVersion:
-        <span>{{ chromeVersion }}</span>
+    <div class="shadow-lg rounded-lg p-5 bg-white flex flex-col gap-4">
+      <div class="flex items-center justify-between gap-3">
+        <LucideCompass />
+        <span class="flex-1 min-w-[120px]">ChromeVersion:</span>
+        <span class="felx-1 text-green-500">{{ chromeVersion }}</span>
       </div>
-      <ElButton type="primary">Ping</ElButton>
+      <div class="flex items-center justify-between gap-3">
+        <LucideHexagon />
+        <span class="flex-1 min-w-[120px]">NodeVersion:</span>
+        <span class="flex-1 text-green-500">{{ nodeVersion }}</span>
+      </div>
+      <div class="flex items-center justify-between gap-3">
+        <LucideCable />
+        <span class="flex-1 min-w-[120px]">ElectronVersion:</span>
+        <span class="flex-1 text-green-500">{{ electronVersion }}</span>
+      </div>
+      <ElButton type="primary" @click="handlePing">Ping</ElButton>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ElButton } from "element-plus";
+import { ElButton, ElMessage } from "element-plus";
+import { LucideCompass, LucideHexagon, LucideCable } from "lucide-vue-next";
 const chromeVersion = window.versions.chrome();
+const nodeVersion = window.versions.node();
+const electronVersion = window.versions.electron();
+
+const handlePing = async () => {
+  let result = await window.api.ping();
+  ElMessage({
+    message: result,
+    type: "success",
+    duration: 1000,
+  });
+};
 </script>
