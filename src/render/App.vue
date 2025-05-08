23207e49463d16dd116558295440089b40940a00
hhl206 <hhl206@qq.com>
2025-05-08 18:04:21 +0800

    更新 ESLint 配置以支持 Vue 3 和 TypeScript，添加新的 lint 脚本；修复 App.vue 中的拼写错误，优化按钮布局；更新 tsconfig.json 以包含 node 类型

--- src/render/App.vue
+++ src/render/App.vue
@@ -4,7 +4,7 @@
       <div class="flex items-center justify-between gap-3">
         <LucideCompass />
         <span class="flex-1 min-w-[120px]">ChromeVersion:</span>
-        <span class="felx-1 text-green-500">{{ chromeVersion }}</span>
+        <span class="flex-1 text-green-500">{{ chromeVersion }}</span>
       </div>
       <div class="flex items-center justify-between gap-3">
         <LucideHexagon />
@@ -16,7 +16,9 @@
         <span class="flex-1 min-w-[120px]">ElectronVersion:</span>
         <span class="flex-1 text-green-500">{{ electronVersion }}</span>
       </div>
-      <ElButton type="primary" @click="handlePing">Ping</ElButton>
+      <ElButton type="primary" @click="handlePing">
+        Ping
+      </ElButton>
     </div>
   </div>
 </template>
@@ -24,12 +26,13 @@
 <script lang="ts" setup>
 import { ElButton, ElMessage } from "element-plus";
 import { LucideCompass, LucideHexagon, LucideCable } from "lucide-vue-next";
+
 const chromeVersion = window.versions.chrome();
 const nodeVersion = window.versions.node();
 const electronVersion = window.versions.electron();
 
 const handlePing = async () => {
-  let result = await window.api.ping();
+  const result = await window.api.ping();
   ElMessage({
     message: result,
     type: "success",
