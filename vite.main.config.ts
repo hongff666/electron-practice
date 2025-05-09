674fa168276d916c26f1ee193f72d50509dbd1bf
hhl206 <hhl206@qq.com>
2025-05-09 11:44:26 +0800

    更新 launch.json 和 vite.main.config.ts，添加调试配置和构建选项以支持源映射

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    build: {
+      sourcemap: isDev,
+      minify: !isDev,
+    },
+  };
+});
